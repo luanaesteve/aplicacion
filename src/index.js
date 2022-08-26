76ada844cdef92ff1ae6cf011fe1fbc5d122c162
luana <luanaesteve73@gmail.com>
2022-08-26 20:18:28 -0300

    uniendo con auth0

--- src/index.js
+++ src/index.js
@@ -3,11 +3,16 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {Auth0Provider} from '@auth0/auth0-react';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+  <Auth0Provider domain='dev-j--zm-2n.us.auth0.com' 
+  clientId='WRZ9S4WoeGRH2jsZKBrhAGAXYx608scY' 
+  redirectUri={window.location.origin}>
+    <App/>
+  </Auth0Provider>
   </React.StrictMode>
 );
 
