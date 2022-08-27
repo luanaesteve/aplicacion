36420b96f07077439649a2b49539798e559b5eab
luana <luanaesteve@hotmail.com>
2022-08-27 00:41:53 -0300

    gkj

--- aplicacion/App.js
+++ aplicacion/App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>nashe</Text>
+      <Text>nasheeeeee</Text>
       <StatusBar style="auto" />
     </View>
   );
