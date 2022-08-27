cfb84afbafc248e892ac6043708decd6f5bfdbaa
luana <luanaesteve@hotmail.com>
2022-08-27 00:48:39 -0300

    naswhe

--- aplicacion/App.js
+++ aplicacion/App.js
@@ -1,10 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>nasheeeeee</Text>
+      <Text></Text>
       <StatusBar style="auto" />
     </View>
   );
