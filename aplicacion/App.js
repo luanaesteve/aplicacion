25ba01211f86f373a8b443f44aa93fcbd66b2962
luana <luanaesteve73@gmail.com>
2022-08-29 17:36:25 -0300

    gjkg

--- aplicacion/App.js
+++ aplicacion/App.js
@@ -1,44 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, TextInput, Dimensions, Image } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Dimensions } from 'react-native';
+import login from '../src/UI/Components/login';
 
 export default function App() {
   return(
-    <View style={styles.container}>
-      <Text style={styles.titulo}> NASHE</Text>
-      <TextInput style={styles.TextInput}
-        placeholder="Email"
-      />
-      <TextInput style={styles.TextInput}
-        placeholder="Contraseña"
-      />
+    <View>
       <StatusBar style="auto" />
+    <login></login>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  titulo:{
-  fontSize:80,
-  },
-
-  TextInput: {
-    borderWidth: 1,
-    borderColor: 'green',
-    padding: 10,
-    width: '80%',
-    height: 50,
-    marginTop: 20,
-    borderRadius: 20,
-  },
-  ImagenSuperior: {
-  position: 'absolute',
-  top: 50,
-
-  }
-});
